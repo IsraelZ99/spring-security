bbb766ff816d983d6e8d5c9ce7768eb61aaec028
Israel Garcia <israel.garcia@decsef.com>
2021-06-02 10:13:02 -0500

    Finish 4- Permission Based Authentication

--- src/main/java/com/example/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/security/ApplicationSecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.security.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,9 +14,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.security.security.ApplicationUserPermission.COURSE_WRITE;
+import static com.example.security.security.ApplicationUserRole.*;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -25,33 +31,47 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable() //TODO I will teach in detail in the next section.
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*")
-                .permitAll()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+//                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic(); // Basic authentication.
     }
 
+    /** User in memory with encrypt password (Class PasswordConfig to set the type of encode). **/
     @Override
     @Bean
-    /** User in memory with encrypt password (Class PasswordConfig to set the type of encode). **/
     protected UserDetailsService userDetailsService() {
         UserDetails israelUser = User.builder()
-                .username("israel.garcia")
+                .username("israel")
                 .password(passwordEncoder.encode("password"))
-                .roles("STUDENT") //ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
+//                .roles(STUDENT.name()) // ROLE_STUDENT
                 .build();
 
         UserDetails monseUser = User.builder()
                 .username("monserrat")
                 .password(passwordEncoder.encode("12345"))
-                .roles("ADMIN")
+                .authorities(ADMIN.getGrantedAuthorities())
+//                .roles(ADMIN.name()) // ROLE_ADMIN
+                .build();
+
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("12345"))
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+//                .roles(ADMINTRAINEE.name()) //ROLE_ADMINTRAINEE
                 .build();
 
         return new InMemoryUserDetailsManager(
-                israelUser
+                israelUser, monseUser, tomUser
         );
     }
 }
